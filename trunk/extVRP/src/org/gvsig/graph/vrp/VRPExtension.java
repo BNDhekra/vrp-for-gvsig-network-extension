8f88bbf3aff02966b487c4ae13e7c6c9ea6ec8b9
fpenarru@gmail.com <fpenarru@gmail.com@9570f040-fa2c-cbde-b1be-cb7465dec4ef>
2012-06-13 16:26:16 +0000

    Plugin to solve Vehicle Routing Problem (isolated from extGraph and depending on this plugin)

---
+++ trunk/extVRP/src/org/gvsig/graph/vrp/VRPExtension.java
@@ -0,0 +1,189 @@
+/* gvSIG. Sistema de Información Geográfica de la Generalitat Valenciana
+ *
+ * Copyright (C) 2004 IVER T.I. and Generalitat Valenciana.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ *
+ * For more information, contact:
+ *
+ *  Generalitat Valenciana
+ *   Conselleria d'Infraestructures i Transport
+ *   Av. Blasco Ibáñez, 50
+ *   46010 VALENCIA
+ *   SPAIN
+ *
+ *      +34 963862235
+ *   gvsig@gva.es
+ *      www.gvsig.gva.es
+ *
+ *    or
+ *
+ *   IVER T.I. S.A
+ *   Salamanca 50
+ *   46005 Valencia
+ *   Spain
+ *
+ *   +34 963163400
+ *   dac@iver.es
+ */
+package org.gvsig.graph.vrp;
+
+import java.awt.Component;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import org.gvsig.exceptions.BaseException;
+import org.gvsig.graph.IODMatrixFileWriter;
+import org.gvsig.graph.ODMatrixFileWriter4cols;
+import org.gvsig.graph.ODMatrixFileWriter4cols_minutes_km;
+import org.gvsig.graph.ODMatrixFileWriterRFormat;
+import org.gvsig.graph.core.GraphException;
+import org.gvsig.graph.core.Network;
+import org.gvsig.graph.vrp.gui.VRPControlPanel;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.plugins.Extension;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.cit.gvsig.fmap.MapContext;
+import com.iver.cit.gvsig.fmap.MapControl;
+import com.iver.cit.gvsig.fmap.layers.FLayer;
+import com.iver.cit.gvsig.fmap.layers.SingleLayerIterator;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
+
+/**
+ * @author Francisco José Peñarrubia (fjp@scolab.es)
+ *
+ * ODMatrixControlPanel sets selectedWriter to allow multiple export formats
+ */
+public class VRPExtension extends Extension {
+	
+	private static MapControl mapControl;
+
+	private static ArrayList<IODMatrixFileWriter> odMatrixWriters = new ArrayList<IODMatrixFileWriter>();
+
+	private static IODMatrixFileWriter selectedWriter;
+	
+	public static void registerOdMatrixFormat(IODMatrixFileWriter w) {
+		odMatrixWriters.add(w);
+	}
+	
+	public static IODMatrixFileWriter[] getOdMatrixWriters() {
+		return (IODMatrixFileWriter[]) odMatrixWriters.toArray(new IODMatrixFileWriter[0]);
+	}
+	
+	public void initialize() {
+		PluginServices.getIconTheme().registerDefault(
+				"vrp",
+				this.getClass().getClassLoader().getResource("images/vrp.png")
+			);
+		
+		ODMatrixFileWriter4cols f1 = new ODMatrixFileWriter4cols();
+		ODMatrixFileWriter4cols_minutes_km f2 = new ODMatrixFileWriter4cols_minutes_km();
+		ODMatrixFileWriterRFormat f3 = new ODMatrixFileWriterRFormat();
+		registerOdMatrixFormat(f1);
+		registerOdMatrixFormat(f2);
+		registerOdMatrixFormat(f3);
+	}
+
+	public void execute(String actionCommand) {
+		if (actionCommand.equals("VRP")) {
+			Network net = getNetwork();
+
+			if ( net != null)
+			{
+//OdMatrixControlPanel ctrlDlg = new OdMatrixControlPanel();
+VRPControlPanel ctrlDlg = new VRPControlPanel(odMatrixWriters, net, this);
+					try {
+						ctrlDlg.setMapContext(mapControl.getMapContext());
+						PluginServices.getMDIManager().addWindow(ctrlDlg);
+
+					} catch (BaseException e) {
+						e.printStackTrace();
+						if (e.getCode() == GraphException.FLAG_OUT_NETWORK) {
+							JOptionPane.showMessageDialog((Component) PluginServices.getMainFrame(), PluginServices.getText(null, "there_are_points_outside_the_tolerance"));
+//							NotificationManager.addError(e.getFormatString(), e);
+					}
+				}
+			}
+		}
+	}
+	
+	// Is this extension enabled?
+	// It is enabled when there is a network on some Feature Layer
+	public boolean isEnabled() {
+		Network net = getNetwork();
+		if ( net != null)
+		{
+			return true;
+		}
+		else 
+			return false;
+	}
+
+
+	public boolean isVisible() {
+		IWindow f = PluginServices.getMDIManager().getActiveWindow();
+		if (f == null) {
+		    return false;
+		}
+		if (f instanceof View) {
+			return true;
+		}
+		return false;
+
+	}
+	
+	/*
+	 * Getters and Setters
+	 */
+	// Returns a network defined on a layer
+	public static Network getNetwork(){
+		IWindow window = PluginServices.getMDIManager().getActiveWindow();
+		if (window instanceof View)
+		{
+			View v = (View) window;
+	        mapControl = v.getMapControl();
+			MapContext map = mapControl.getMapContext();
+			
+			SingleLayerIterator it = new SingleLayerIterator(map.getLayers());
+			while (it.hasNext())
+			{
+				FLayer aux = it.next();
+				if (!aux.isActive())
+					continue;
+				Network net = (Network) aux.getProperty("network");
+				return net;
+			}
+		}
+		return null;
+	}
+	
+	public MapControl getMapControl(){
+		return mapControl;
+	}
+
+
+	public static IODMatrixFileWriter getSelectedWriter() {
+		return selectedWriter;
+	}
+
+	public static void setSelectedWriter(IODMatrixFileWriter selectedWriter) {
+		VRPExtension.selectedWriter = selectedWriter;
+	}
+
+}
+
+
---
+++ trunk/extVRP/src/org/gvsig/graph/vrp/gui/Results_Preview.java
@@ -0,0 +1,271 @@
+package org.gvsig.graph.vrp.gui;
+
+import javax.swing.AbstractButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.andami.ui.mdiManager.WindowInfo;
+import com.iver.cit.gvsig.fmap.MapContext;
+import com.iver.cit.gvsig.fmap.MapControl;
+import com.iver.cit.gvsig.fmap.core.IFeature;
+import com.iver.cit.gvsig.fmap.core.IGeometry;
+import com.iver.cit.gvsig.fmap.core.styles.ArrowDecoratorStyle;
+import com.iver.cit.gvsig.fmap.core.styles.ILineStyle;
+import com.iver.cit.gvsig.fmap.core.styles.SimpleLineStyle;
+import com.iver.cit.gvsig.fmap.core.symbols.ArrowMarkerSymbol;
+import com.iver.cit.gvsig.fmap.core.symbols.SimpleLineSymbol;
+import com.iver.cit.gvsig.fmap.drivers.MemoryDriver;
+import com.iver.cit.gvsig.fmap.layers.GraphicLayer;
+import com.iver.cit.gvsig.fmap.rendering.FGraphic;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.swing.JTextField;
+
+import org.cresques.cts.ProjectionPool;
+import org.gvsig.graph.core.GraphException;
+import org.gvsig.graph.core.GvFlag;
+import org.gvsig.graph.core.Network;
+import org.gvsig.graph.solvers.Route;
+import org.gvsig.graph.solvers.ShortestPathSolverAStar;
+import org.gvsig.graph.vrp.support.Nodes;
+import org.metavrp.GA.Chromosome;
+import org.metavrp.GA.Gene;
+
+public class Results_Preview extends JPanel implements IWindow, Runnable {
+	
+	private VRPControlPanel controlPanel;				// The VRP Control Panel that called this object
+	
+	// The window that this panel uses (where it is painted)
+	private WindowInfo wi;
+	
+	// The thread that runs this window contents
+	private Thread previewThread;
+	private JTextField txtTeste;
+	
+	// The contents of this Panel
+	
+	
+	// Constructor
+	public Results_Preview(VRPControlPanel controlPanel) {
+		this.controlPanel = controlPanel;
+		initComponents();
+	}
+
+	// Initialize this JPanel
+	private void initComponents() {
+		setSize(new Dimension(490, 340));
+		setPreferredSize(new Dimension(490, 340));				//Preferred Size
+		setLayout(null);
+		
+		txtTeste = new JTextField();
+		txtTeste.setBounds(192, 138, 86, 20);
+		txtTeste.setText("teste");
+//		add(txtTeste);
+		txtTeste.setColumns(10);
+//	    setUndecorated(true);									//No decorations (border, buttons, etc)
+//	    getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+
+	}
+
+	public WindowInfo getWindowInfo() {
+		if (wi == null) {
+			wi = new WindowInfo(WindowInfo.RESIZABLE | WindowInfo.MAXIMIZABLE | WindowInfo.ICONIFIABLE | WindowInfo.MODELESSDIALOG | WindowInfo.NOTCLOSABLE);
+			wi.setWidth((int) this.getPreferredSize().getWidth());
+			wi.setHeight((int) this.getPreferredSize().getHeight());
+			refreshWindowInfo();
+			wi.setTitle("VRP Solution Preview");
+		}
+		return wi;
+	}
+	
+	// Refreshes the current WindowInfo with the current coordinates of the main window
+	public void refreshWindowInfo(){
+		// Get the sizes of the mother window
+		int width = controlPanel.getWindowInfo().getWidth();
+//		int height = controlPanel.getWindowInfo().getHeight();
+		int X = controlPanel.getWindowInfo().getX() + width;
+		int Y = controlPanel.getWindowInfo().getY();
+		// Put them on this window
+		wi.setX(X);
+		wi.setY(Y);
+	}
+
+	public Object getWindowProfile() {
+//		return WindowInfo.EDITOR_PROFILE;
+//		return WindowInfo.TOOL_PROFILE;
+//		return WindowInfo.PROJECT_PROFILE;
+//		return WindowInfo.PROPERTIES_PROFILE;
+		return WindowInfo.DIALOG_PROFILE;
+	}
+	
+	
+	// Runs this Preview window, showing the best result of the population
+	public void run(){
+System.out.println("O thread WindowInfo iniciou!");				
+        // From 500ms to 500ms, update the statistics and the graphic
+        while(true){
+//        	synchronized (this){
+//	        	try{
+	            	showBestResult();	// Shows the best element of the population
+
+//	            	wait(15000);			// Wait 500ms
+	        	}
+//	        	catch(InterruptedException e){
+//	        		e.printStackTrace();
+//	        	}
+//        	}
+//        }
+	}
+	
+	// Shows the best element of the population in the content of the window
+	public void showBestResult(){
+		//1. Get the network
+		Network net = controlPanel.getNetwork();
+		
+		//2. Save the original flags
+		GvFlag[] flagsBackup = net.getFlags();
+
+		//3. Remove all the flags from the network
+		net.removeFlags();
+		
+		//3. Get the best element from the population
+		Chromosome bestChromosome = controlPanel.getResults().getBestElement();
+		
+		//4. Measure the best routes (the shortest paths) between every two pairs of points
+		// TODO:Verificar o caso do veículo ter que regressar à base antes do próximo veículo
+		// TODO:Verificar também o caso do veiculo regressar à base para descarregar, por estar cheio
+		ArrayList<Route> routes = calculateRoutes(bestChromosome, net);
+		
+		//5. Draw this routes on the map
+		// TODO: Desenhar com cores diferentes para veículos diferentes
+		draw(routes);
+		
+		//6. Desenhar os pontos da layer de pontos nesse mapa
+		// TODO:
+		
+		//7. Repôr flags originais na rede
+		net.removeFlags();
+		for (GvFlag flag : flagsBackup){
+			net.addFlag(flag);
+		}
+	}
+	
+	// Calculates all the Routes between every two points on a Network
+	public ArrayList<Route> calculateRoutes(Chromosome bestChromosome, Network net){
+		ArrayList<Route> routes = new ArrayList<Route>();
+		Nodes nodes = controlPanel.getNodes();
+		
+		Gene geneA, geneB;
+		GvFlag flagA, flagB;
+		int lenght = bestChromosome.getLenght();
+		for (int i=1; i<lenght; i++){
+System.out.println("\n\ni = "+i);			
+			geneA = bestChromosome.getGene(i-1);
+System.out.println("GeneA: " + geneA.toString());			
+			geneB = bestChromosome.getGene(i);
+System.out.println("GeneB: " + geneB.toString());
+			
+			flagA = nodes.getNode(geneA).getFlag();
+System.out.println("FlagA: " + flagA.toString());
+			flagB = nodes.getNode(geneB).getFlag();
+System.out.println("FlagB: " + flagB.toString());
+			
+			routes.add(calculateRoute(flagA, flagB, net));
+		}
+
+		return routes;
+	}
+	
+	// Calculates the Route between two points on a Network
+	// TODO: testar a performance do Dijkstra vs AStar
+	public Route calculateRoute(GvFlag flag1, GvFlag flag2, Network net){
+		ShortestPathSolverAStar solver = new ShortestPathSolverAStar();
+		Route route = null;
+		
+		try{
+			net.removeFlags();
+			solver.setNetwork(net);
+			net.addFlag(flag1);
+			net.addFlag(flag2);
+			String fieldStreetName = (String) net.getLayer().getProperty("network_fieldStreetName");
+			solver.setFielStreetName(fieldStreetName);
+			route = solver.calculateRoute();
+		} catch (GraphException ex){
+			ex.printStackTrace();
+		}
+		if (route.getFeatureList().size() == 0)
+		{
+			JOptionPane.showMessageDialog((JComponent) PluginServices.getMDIManager().getActiveWindow(),
+					PluginServices.getText(this, "shortest_path_not_found"));
+		}
+		
+		return route;
+	}
+	
+		
+	// Given some collection of features, draw its graphics on a MapControl
+	private void createGraphicsFrom(Collection featureList, MapControl mapControl) {
+		Iterator it = featureList.iterator();
+		GraphicLayer graphicLayer = mapControl.getMapContext().getGraphicsLayer();
+		SimpleLineSymbol arrowSymbol = new SimpleLineSymbol();
+		arrowSymbol.setLineWidth(3.0f);
+		ILineStyle lineStyle = new SimpleLineStyle();
+		ArrowDecoratorStyle arrowDecoratorStyle = new ArrowDecoratorStyle();
+		ArrowMarkerSymbol marker = (ArrowMarkerSymbol) arrowDecoratorStyle.getMarker(); 
+		marker.setSize(16);
+		marker.setColor(Color.RED);
+		arrowDecoratorStyle.setArrowMarkerCount(1);
+		lineStyle.setArrowDecorator(arrowDecoratorStyle );
+		lineStyle.setLineWidth(3.0f);
+		arrowSymbol.setLineColor(Color.RED);
+		arrowSymbol.setAlpha(120);
+		arrowSymbol.setLineStyle(lineStyle);
+		int idSymbolLine = graphicLayer.addSymbol(arrowSymbol);
+
+//TODO:translate the following statement
+		// Para evitar hacer reallocate de los elementos de la
+		// graphicList cada vez, creamos primero la lista
+		// y la insertamos toda de una vez.
+		ArrayList<FGraphic> graphicsRoute = new ArrayList<FGraphic>();
+		while (it.hasNext()) {
+			IFeature feat = (IFeature) it.next();
+			IGeometry gAux = feat.getGeometry();
+			FGraphic graphic = new FGraphic(gAux, idSymbolLine);
+			graphic.setTag("ROUTE");
+			graphicsRoute.add(graphic);
+			graphicLayer.insertGraphic(0, graphic);
+		}
+		// Lo insertamos al principio de la lista para que los
+		// pushpins se dibujen después.
+
+		graphicLayer.inserGraphics(0, graphicsRoute);
+		mapControl.drawGraphics();
+	}
+	
+	// Draw the routes on the overview map
+	public void draw(ArrayList<Route> routes){
+//		MapControl mapCtrl = new MapControl();
+		MapControl mapCtrl = controlPanel.getMapControl();
+		this.add(mapCtrl, java.awt.BorderLayout.CENTER);
+		for (Route route:routes){
+			createGraphicsFrom(route.getFeatureList(), mapCtrl);
+		}
+//		mapCtrl.setProjection(controlPanel.getProjection());
+//		mapCtrl.setSize(this.getWidth(), this.getHeight());
+//		mapCtrl.commandRepaint();
+	}
+}
